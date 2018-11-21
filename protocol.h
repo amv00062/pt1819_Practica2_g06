5e7410b024a5442e3f88c3ba5b477d4d56a4ddcf
amv00062 <amv00062@red.ujaen.es>
2018-11-21 19:11:15 +0100

    inicial

---
+++ protocol.h
@@ -0,0 +1,37 @@
+#ifndef protocolostpte_practicas_headerfile
+#define protocolostpte_practicas_headerfile
+#endif
+
+// COMANDOS DE APLICACION
+#define HELO "HELO"  // 
+
+#define QUIT  "QUIT" // Finalizacion de la conexion de aplicacion
+
+
+// RESPUESTAS A COMANDOS DE APLICACION
+#define OK  "2"
+#define OKDATA "4"
+#define ER  "5"
+#define OKW "250"
+
+//FIN DE RESPUESTA
+#define CRLF "\r\n"
+
+//ESTADOS
+#define S_WELC 0
+#define S_HELO 1
+#define S_MAIL 2
+#define S_RCPT 3
+#define S_DATA 4
+#define S_QUIT 5
+#define S_EXIT 6
+
+//PUERTO DEL SERVICIO
+#define TCP_SERVICE_PORT  25
+
+
+
+// #define mod "mod"
+#define MF "mail from:"
+#define RCPT "rcpt to:"
+#define DATA "data:"
