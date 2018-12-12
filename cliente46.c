99cd11e838e5c4f05640d4df7545e3bf8268d3f0
angel moreno <er_angel_97@hotmail.com>
2018-12-12 18:40:41 +0100

    para willy

--- cliente46.c
+++ cliente46.c
@@ -33,11 +33,12 @@ int main(int *argc, char *argv[])
 	int estado=S_HELO;
 	char option;
 	int ipversion=AF_INET;//IPv4 por defecto
-	char ipdest[256],ipdestl;
+	char ipdest[256];
 	char default_ip4[16]="127.0.0.1";
 	char default_ip6[64]="::1";
 	
-
+	unsigned long ipdestl;
+	struct in_addr address;
 
 	WORD wVersionRequested;
 	WSADATA wsaData;
@@ -80,19 +81,19 @@ int main(int *argc, char *argv[])
 			printf("CLIENTE> Introduzca la IP destino (pulsar enter para IP por defecto):\r\n ");
 			gets_s(ipdest,sizeof(ipdest));
 
-
-		/*	ipdestl = inet_addr(ipdest);
+			ipdestl = inet_addr(ipdest);
 			if (ipdestl == INADDR_NONE) {
+				//La dirección introducida por teclado no es correcta o
+				//corresponde con un dominio.
 				struct hostent *host;
 				host = gethostbyname(ipdest);
 				if (host != NULL) {
-				
-					memcpy(&ipdest, host->h_addr_list[0], 4);
-					//printf("\nDirecion %s\n", inet_ntoa(ipdestl));
+					memcpy(&address, host->h_addr_list[0], 4);
+					printf("\nDireccion %s\n", inet_ntoa(address));
+					strcpy(ipdest, inet_ntoa(address));
 				}
-				
 			}
-			*/
+
 		
 			//Dirección por defecto según la familia
 			if(strcmp(ipdest,"")==0 && ipversion==AF_INET)
